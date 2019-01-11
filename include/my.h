545b87e6fa38794b1a49a12b62a8f332b8ada154
Wang Marc <Mwaaaa@localhost.localdomain>
2019-01-11 16:27:05 +0100

    some cleaning

--- include/my.h
+++ include/my.h
@@ -14,11 +14,6 @@ int display_pal(char *str);
 int my_putstr(char const *str);
 void my_putchar(char c);
 int my_strlen(char const *str);
-int my_getnbr(char const *str);
-char *my_strcpy(char *dest, char const *src);
-char *my_revstr(char *str);
 int my_put_nbr(int nb);
-int number(char *str);
-void my_printf(char *str, ...);
 
 #endif
